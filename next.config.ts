e92e46771628e0a2e0d9e01db06170dac6c11fbc
Teek4thuch <teekubp.p@gmail.com>
2026-04-03 14:29:52 +0700

    add delpoy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/ci_cd_practice",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
